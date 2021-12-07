c8d3c2ca351c914cd092674535169a3fb74c25c6
Marco Seghesso <mseghesso@gmail.com>
2021-12-08 07:12:46 +1300

    All elements added including Event Listeners

---
+++ index.js
@@ -0,0 +1,48 @@
+const buttons = document.querySelectorAll(".drum");
+
+for (let i = 0; i < buttons.length; i++) {
+   buttons[i].addEventListener("click", function () {
+      let buttonInnerHTML = this.innerHTML;
+      playSound(buttonInnerHTML);
+   })
+}
+
+document.addEventListener("keypress", function(event){
+   playSound(event.key)
+});
+
+function playSound (character){
+
+   switch (character) {
+      case "a":
+         const crash = new Audio('./sounds/crash.mp3')
+         crash.play()
+         break
+      case "s":
+         const kick = new Audio('./sounds/kick-bass.mp3')
+         kick.play()
+         break
+      case "d":
+         const snare = new Audio('./sounds/snare.mp3')
+         snare.play()
+         break
+      case "f":
+         const tom1 = new Audio('./sounds/tom-1.mp3')
+         tom1.play()
+         break
+      case "j":
+         const tom2 = new Audio('./sounds/tom-2.mp3')
+         tom2.play()
+         break
+      case "k":
+         const tom3 = new Audio('./sounds/tom-3.mp3')
+         tom3.play()
+         break
+      case "l":
+         const tom4 = new Audio('./sounds/tom-4.mp3')
+         tom4.play()
+         break
+      default:
+         console.log(buttonInnerHTML)
+   }
+}
